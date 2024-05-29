315b45efaf1335acd4f58050839191ee17926ae8
Isa <isa.selimovic@stu.ibu.edu.ba>
2024-05-30 01:38:20 +0200

    commitw

--- Project2/js/scripts.js
+++ Project2/js/scripts.js
@@ -5,3 +5,20 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+// scripts.js
+document.addEventListener('DOMContentLoaded', function() {
+    var headers = document.querySelectorAll('.accordion-header');
+
+    headers.forEach(function(header) {
+        header.addEventListener('click', function() {
+            var content = this.nextElementSibling;
+
+            if (content.style.display === 'block') {
+                content.style.display = 'none';
+            } else {
+                content.style.display = 'block';
+            }
+        });
+    });
+});
